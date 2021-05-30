52c8c918f912376053ad0b1ac089e0b63e2a299d
Madmanmayson <dylanhardy97@gmail.com>
2021-05-30 19:34:39 -0400

    Created fat free controller skeletons

--- api.php
+++ api.php
@@ -1,2 +1,19 @@
 <?php
 
+//this is my controller for the pets project
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//require autoload file
+require_once('vendor/autoload.php');
+
+//instantiate fat-free
+$f3 = Base::instance();
+
+//define default route
+$f3->route('GET /api/*', function($f3, $params){
+    echo var_dump($params) . ' Test';
+});
+
+//run fat-free
+$f3->run();
