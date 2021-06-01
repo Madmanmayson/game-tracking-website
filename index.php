b30e483d52f5a9b45c8889ffb4b455ae40825a9a
Madmanmayson <dylanhardy97@gmail.com>
2021-06-01 14:03:48 -0400

    Add navbar and basic routes

--- index.php
+++ index.php
@@ -1,21 +1,30 @@
 <?php
-
-//this is my controller for the pets project
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 //require autoload file
 require_once('vendor/autoload.php');
 
+session_start();
+
 //instantiate fat-free
 $f3 = Base::instance();
 
-//define default route
 $f3->route('GET /', function(){
-    //display the home page
     $view = new Template();
     echo $view->render('views/home.html');
 });
 
-//run fat-free
+$f3->route('GET /profile', function(){
+    //TODO check if the username is set in the session. If so, redirect to their profile, otherwise redirect to login or profile search
+
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->route('GET /search', function(){
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
 $f3->run();
