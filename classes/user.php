a74fd6599855f0ec675db051322c3a923d7a106f
Madmanmayson <dylanhardy97@gmail.com>
2021-06-12 00:33:27 -0400

    Updated user class to new database structure

--- classes/user.php
+++ classes/user.php
@@ -2,10 +2,50 @@
 
 class User
 {
-    private $_userId;
-    private $_userName;
-    private $_password;
-    private $_isAdmin;
+    private $_userId = 0;
+    private $_userName = "";
+    private $_password = "";
+    private $_isAdmin = false;
+    private $_email = "";
+
+    function __construct($databaseRow)
+    {
+        if(isset($databaseRow['userId'])){
+            $this->_userId = $databaseRow['userId'];
+        }
+
+        if(isset($databaseRow['username'])){
+            $this->_userName = $databaseRow['username'];
+        }
+
+        if(isset($databaseRow['email'])){
+            $this->_email = $databaseRow['email'];
+        }
+
+        if(isset($databaseRow['password'])){
+            $this->_password = $databaseRow['password'];
+        }
+
+        if(isset($databaseRow['isAdmin'])){
+            $this->_isAdmin = $databaseRow['isAdmin'];
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->_email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->_email = $email;
+    }
 
     /**
      * @return int
