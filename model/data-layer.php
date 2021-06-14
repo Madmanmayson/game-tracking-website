6f981343ae077f8593b28eee03a358df757dd5ae
Ben Fuqua <bfuqua@mail.greenriver.edu>
2021-06-14 08:21:54 -0400

    Fixed some typos

--- model/data-layer.php
+++ model/data-layer.php
@@ -1,32 +1,31 @@
 <?php
 
 class dataLayer{
-
     public static function getPlatforms(){
         return array(
-            [1] => 'PC',
-            [2] => 'Playstation 3',
-            [3] => 'Playstation 4',
-            [4] => 'Playstation 5',
-            [5] => 'Xbox 360',
-            [6] => 'Xbox One',
-            [7] => 'Xbox Series X',
-            [8] => 'Gamecube',
-            [9] => 'Wii',
-            [10] => 'Wii U',
-            [11] => 'Switch',
-            [12] => 'DS',
-            [13] => '3DS'
+            1 => 'PC',
+            2 => 'Playstation 3',
+            3 => 'Playstation 4',
+            4 => 'Playstation 5',
+            5 => 'Xbox 360',
+            6 => 'Xbox One',
+            7 => 'Xbox Series X|S',
+            8 => 'Gamecube',
+            9 => 'Wii',
+            10 => 'Wii U',
+            11 => 'Switch',
+            12 => 'DS',
+            13 => '3DS'
         );
     }
 
     public static function getStatuses(){
         return array(
-            [1] => 'Backloged',
-            [2] => 'In Progess',
-            [3] => 'Completed',
-            [4] => 'Dropped',
-            [5] => 'On Hold'
+            1 => 'Backlogged',
+            2 => 'In Progress',
+            3 => 'Completed',
+            4 => 'Dropped',
+            5 => 'On Hold'
         );
     }
 }
