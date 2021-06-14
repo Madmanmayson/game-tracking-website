bde49a9dc62117e7d950862ab1021b4216455507
Madmanmayson <dylanhardy97@gmail.com>
2021-06-14 07:25:35 -0400

    Added model code for platform and status lists

---
+++ model/data-layer.php
@@ -0,0 +1,32 @@
+<?php
+
+public class dataLayer{
+
+    public static function getPlatforms(){
+        return array(
+            [1] => 'PC',
+            [2] => 'Playstation 3',
+            [3] => 'Playstation 4',
+            [4] => 'Playstation 5',
+            [5] => 'Xbox 360',
+            [6] => 'Xbox One',
+            [7] => 'Xbox Series X',
+            [8] => 'Gamecube',
+            [9] => 'Wii',
+            [10] => 'Wii U',
+            [11] => 'Switch',
+            [12] => 'DS',
+            [13] => '3DS'
+        );
+    }
+
+    public static function getStatuses(){
+        return array(
+            [1] => 'Backloged',
+            [2] => 'In Progess',
+            [3] => 'Completed',
+            [4] => 'Dropped',
+            [5] => 'On Hold'
+        );
+    }
+}
