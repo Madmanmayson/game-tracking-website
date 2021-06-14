9533648ed65d61ffbd5ea47b7b9d6964dbb7c411
Madmanmayson <dylanhardy97@gmail.com>
2021-06-14 04:40:09 -0400

    Prepared search page for website

--- controller/controller.php
+++ controller/controller.php
@@ -18,102 +18,12 @@ class Controller
 
     function login()
     {
-        $_SESSION = array();
-        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-            $oldPath = $_SERVER['SCRIPT_URI'];
-            $partToRemove = substr($oldPath, strpos($oldPath, 'login'));
-            $apiPath = substr($oldPath, 0, strlen($oldPath) - strlen($partToRemove));
-
-            $curl = curl_init("{$apiPath}api/login");
-
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($_POST));
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            $result = curl_exec($curl);
-            $data = json_decode($result, true);
-            if (!curl_errno($curl)) {
-                switch ($http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE)) {
-                    case 200:  # OK
-                        if ($data['isAdmin']){
-                            $_SESSION['user'] = new Admin($data);
-                        }else{
-                            $_SESSION['user'] = new User($data);
-                        }
-
-                        //route to their profile
-                        header("Location: profile/{$_SESSION['user']->getUserName()}");
-                    default:
-                        //route to an error page
-                        $this->_f3->set('error', $data['message']);
-                }
-            }
-            else{
-                echo curl_error($curl);
-            }
-        }
-
         $view = new Template();
         echo $view->render('views/login.html');
     }
 
-    function logout(){
-        $_SESSION = array();
-        header('location: /game-tracker/');
-    }
-
     function registration()
     {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-            $userName = $_POST['username'];
-            $userPassword = $_POST['password'];
-            $userEmail = $_POST['email'];
-
-            if (!Validation::validUserName($userName)){
-                $this->_f3->set('errors["username"]',
-                "Please enter an alphanumeric username between 4 and 32 characters");
-            }
-
-            if (!Validation::validPassword($userPassword)){
-                $this->_f3->set('errors["password"]',
-                "Please enter a alphanumeric password at least 6 characters long");
-            }
-
-            if (!Validation::validEmail($userEmail)){
-                $this->_f3->set('errors["email"]',
-                "Please enter a valid email address");
-            }
-
-            if (empty($this->_f3->get('errors')))
-            {
-                $oldPath = $_SERVER['SCRIPT_URI'];
-                $partToRemove = substr($oldPath, strpos($oldPath, 'registration'));
-                $apiPath = substr($oldPath, 0, strlen($oldPath) - strlen($partToRemove));
-
-                $curl = curl_init("{$apiPath}api/users");
-
-                curl_setopt($curl, CURLOPT_POST, true);
-                curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($_POST));
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-                $result = curl_exec($curl);
-                if (!curl_errno($curl)) {
-                    switch ($http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE)) {
-                        case 201:  # OK
-                            //route to their profile
-                            header("Location: profile/{$_POST['username']}");
-                        default:
-                            //route to an error page
-                            //Back to registration for now
-                            echo $result;
-                            $view = new Template();
-                            echo $view->render('views/registration.html');
-                    }
-                }
-                else{
-                    echo curl_error($curl);
-                }
-            }
-        }
-
         $view = new Template();
         echo $view->render('views/registration.html');
     }
@@ -125,7 +35,7 @@ class Controller
         $partToRemove = substr($oldPath, strpos($oldPath, 'profile'));
         $apiPath = substr($oldPath, 0, strlen($oldPath) - strlen($partToRemove));
 
-        $curl = curl_init("{$apiPath}api/users/{$username}");
+        $curl = curl_init("{$apiPath}api/user/{$username}");
 
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         //curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -144,6 +54,6 @@ class Controller
     function search()
     {
         $view = new Template();
-        echo $view->render('views/home.html');
+        echo $view->render('views/search.html');
     }
 }
